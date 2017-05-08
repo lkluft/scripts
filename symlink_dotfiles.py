2b1ede499d61cf14ab00478294a1e15c7b7ad9c8
Lukas Kluft <lukas.kluft@gmail.com>
2017-05-08 16:04:31 +0200

    Create symlink for dotfiles in git repository.

---
+++ symlink_dotfiles.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 Lukas Kluft <lukas.kluft@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Conveniently symlinks dotfiles in user's home directory.
+"""
+import os
+from sys import platform
+
+
+home = os.getenv('HOME')
+gitrepo = os.path.join(home, 'git')
+
+dotfiles = [
+    'bash_aliases',
+    'bash_completion',
+    'bash_functions',
+    'bashrc',
+    'condarc',
+    'ctags',
+    'dircolors',
+    'gitconfig',
+    'inputrc',
+    'msmtprc',
+    'pythonrc',
+    'tmux.conf',
+]
+
+dotdirectories = [
+    'vim',
+    'scripts',
+]
+
+# Symlink common dotfiles.
+for f in dotfiles:
+    src = os.path.join(gitrepo, f)
+    dst = os.path.join(home, '.' + f)
+    # os.symlink(src, dst)
+    print(src, dst)
+
+# Symlink SSH configuration.
+sshdir = os.path.join(home, '.ssh')
+if not os.path.isdir(sshdir):
+    os.mkdir(sshdir)
+src = os.path.join(gitrepo, 'ssh_config')
+dst = os.path.join(sshdir, 'config')
+# os.symlink(src, dst)
+print(src, dst)
+
+# Symlink mail settings depending on operatins system.
+if platform == "linux" or platform == "linux2":  # linux
+    mailrc = 'mail.rc'
+elif platform == "darwin":  # OS X
+    mailrc = 'mailrc'
+
+src = os.path.join(gitrepo, mailrc)
+dst = os.path.join(home, '.' + mailrc)
+# os.symlink(src, dst)
+print(src, dst)
+
+# Symlink scripts directory.
+for d in dotdirectories:
+    src = os.path.join(gitrepo, d)
+    dst = os.path.join(home, '.' + d)
+    # os.symlink(src, dst)
+    print(src, dst)
