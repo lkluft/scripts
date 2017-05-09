70ea25cfbe6d449dad22b4b9be2fa6c55c70a5e2
Lukas Kluft <lukas.kluft@gmail.com>
2017-05-09 15:54:00 +0200

    * symlink_dotfiles.py: Fixed path to config files.
    Script is now actually creating symlinks instead of printing debug
    information.

--- symlink_dotfiles.py
+++ symlink_dotfiles.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 #
 # Copyright © 2017 Lukas Kluft <lukas.kluft@gmail.com>
 #
 # Distributed under terms of the MIT license.
-
 """Conveniently symlinks dotfiles in user's home directory.
 """
 import os
 from sys import platform
 
+
+logger = logging.getLogger()
 
 home = os.getenv('HOME')
 gitrepo = os.path.join(home, 'git')
@@ -25,6 +26,7 @@ dotfiles = [
     'inputrc',
     'msmtprc',
     'pythonrc',
+    'profile',
     'tmux.conf',
 ]
 
@@ -35,19 +37,18 @@ dotdirectories = [
 
 # Symlink common dotfiles.
 for f in dotfiles:
-    src = os.path.join(gitrepo, f)
+    src = os.path.join(gitrepo, 'config_files', f)
     dst = os.path.join(home, '.' + f)
-    # os.symlink(src, dst)
-    print(src, dst)
+    os.symlink(src, dst)
 
 # Symlink SSH configuration.
 sshdir = os.path.join(home, '.ssh')
 if not os.path.isdir(sshdir):
+    # Ensure that the .ssh directory is present.
     os.mkdir(sshdir)
 src = os.path.join(gitrepo, 'ssh_config')
 dst = os.path.join(sshdir, 'config')
-# os.symlink(src, dst)
-print(src, dst)
+os.symlink(src, dst)
 
 # Symlink mail settings depending on operatins system.
 if platform == "linux" or platform == "linux2":  # linux
@@ -57,12 +58,10 @@ elif platform == "darwin":  # OS X
 
 src = os.path.join(gitrepo, mailrc)
 dst = os.path.join(home, '.' + mailrc)
-# os.symlink(src, dst)
-print(src, dst)
+os.symlink(src, dst)
 
 # Symlink scripts directory.
 for d in dotdirectories:
     src = os.path.join(gitrepo, d)
     dst = os.path.join(home, '.' + d)
-    # os.symlink(src, dst)
-    print(src, dst)
+    os.symlink(src, dst)
